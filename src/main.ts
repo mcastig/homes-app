4afa85e5092c3c1d2ec1531350d687b73eae01ac
Maximino Castillo <maxcastgarcia@gmail.com>
2024-11-12 16:21:58 -0600

    Add standalone router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
+import { DetailsComponent } from './app/details/details.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'details/:id', component: DetailsComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
